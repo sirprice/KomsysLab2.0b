903bf5cb08746973053c513eb7a0357ed19a9a61
magnus-1 <magnus-1@users.noreply.github.com>
2016-10-05 14:27:11 +0200

    invoke cases

---
+++ src/Invoker.java
@@ -0,0 +1,45 @@
+import states.ClientSipState;
+
+/**
+ * Created by o_0 on 2016-10-05.
+ */
+public class Invoker {
+    public static class InvokeInvite implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveInvite(body);
+        }
+    }
+    public static class InvokeTRO implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveTRO();
+        }
+    }
+    public static class InvokeBye implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveBye();
+        }
+    }
+    public static class InvokeOK implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveOk();
+        }
+    }
+    public static class InvokeAck implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveAck();
+        }
+    }
+
+    public static class InvokeInvalid implements SignalInvoker {
+        @Override
+        public ClientSipState invoke(ClientSipState state, String body) {
+            return state.recieveInvalid(body);
+        }
+    }
+
+}
--- src/states/ClientSipState.java
+++ src/states/ClientSipState.java
@@ -7,7 +7,8 @@ public abstract class ClientSipState {
 
     public ClientSipState recieveInvite(String body){return this;}
     public ClientSipState recieveTRO(){return this;}
-    public ClientSipState recieveBuy(){return this;}
+    public ClientSipState recieveBye(){return this;}
     public ClientSipState recieveOk(){return this;}
     public ClientSipState recieveAck(){return this;}
+    public ClientSipState recieveInvalid(String body){return this;}
 }
