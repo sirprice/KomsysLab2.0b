f591f1e0027dc4b355afa5f4b203b5c828a79f4e
magnus-1 <magnus-1@users.noreply.github.com>
2016-10-07 12:58:37 +0200

    closing conncetions

--- src/phoneapp/Invoker.java
+++ src/phoneapp/Invoker.java
@@ -3,6 +3,7 @@ package phoneapp;
 
 import phoneapp.states.ClientSipState;
 
+import java.io.IOException;
 import java.net.Socket;
 
 /**
@@ -11,45 +12,45 @@ import java.net.Socket;
 public class Invoker {
     public static class InvokeInvite implements SignalInvoker{
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveInvite(fromSocket,body);
         }
     }
     public static class InvokeTRO implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveTRO();
         }
     }
     public static class InvokeBye implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveBye();
         }
     }
     public static class InvokeOK implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveOk();
         }
     }
     public static class InvokeAck implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveAck();
         }
     }
 
     public static class InvokeBusy implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveBusy();
         }
     }
 
     public static class InvokeInvalid implements SignalInvoker {
         @Override
-        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) {
+        public ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException {
             return state.recieveInvalid(body);
         }
     }
--- src/phoneapp/SignalInvoker.java
+++ src/phoneapp/SignalInvoker.java
@@ -2,11 +2,12 @@ package phoneapp;
 
 import phoneapp.states.ClientSipState;
 
+import java.io.IOException;
 import java.net.Socket;
 
 /**
  * Created by o_0 on 2016-10-05.
  */
 public interface SignalInvoker {
-    ClientSipState invoke(Socket fromSocket, ClientSipState state, String body);
+    ClientSipState invoke(Socket fromSocket, ClientSipState state, String body) throws IOException;
 }
